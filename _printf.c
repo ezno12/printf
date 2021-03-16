62c9b4feb77689c53340997e5c0d66df3ae5967a
tennin12 <bassembenyhia12@gmail.com>
2021-03-16 16:13:22 +0100

    printf updated

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+/**
+* _printf - print function.
+*
+* @format: character.
+*
+* Return : printed caracter, int (success).
+*
+*/
+int _printf(const char *format, ...)
+{
+va_list arg;
+unsigned int i, j, check;
+unsigned int len = 0;
+print_t print[] = {{"c", pchar}, {"s", pstr}, {NULL, NULL}};
+va_start(arg, format);
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (0);
+i = 0;
+while (format[i] != '\0')
+{
+if (format[i] == '%' && format[i + 1] != '%')
+{
+j = 0;
+check = 0;
+while (print[j].p != NULL)
+{
+if (format[i + 1] == print[j].print[0])
+{
+len += print[j].p(arg);
+check = 1;
+i++;
+}
+j++;
+}
+if (flag == 0)
+{
+_putchar(format[i]);
+len += 1;
+}
+}
+else if (format[i] == '%' && format[i + 1] == '%')
+{
+_putchar('%');
+i++;
+len += 1;
+}
+else
+{
+_putchar(format[i]);
+len += 1;
+}
+i++;
+}
+va_end(arg);
+return (len);
+}
