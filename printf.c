93c8045266efbe5c854019a1264847dbe0503bed
tennin12 <bassembenyhia12@gmail.com>
2021-03-16 14:23:21 +0100

    update

--- printf.c
+++ printf.c
@@ -10,7 +10,7 @@
 int _printf(const char *format, ...)
 {
 va_list arg;
-unsigned int i, j, flag;
+unsigned int i, j, check;
 unsigned int len = 0;
 print_t print[] = {{"c", pchar}, {"s", pstr}, {NULL, NULL}};
 va_start(arg, format);
@@ -22,13 +22,13 @@ while (format[i] != '\0')
 if (format[i] == '%' && format[i + 1] != '%')
 {
 j = 0;
-flag = 0;
+check = 0;
 while (print[j].p != NULL)
 {
 if (format[i + 1] == print[j].print[0])
 {
-len = len + print[j].p(arg);
-flag = 1;
+len += print[j].p(arg);
+check = 1;
 i++;
 }
 j++;
@@ -36,19 +36,19 @@ j++;
 if (flag == 0)
 {
 _putchar(format[i]);
-len = len + 1;
+len += 1;
 }
 }
 else if (format[i] == '%' && format[i + 1] == '%')
 {
 _putchar('%');
 i++;
-len = len + 1;
+len += 1;
 }
 else
 {
 _putchar(format[i]);
-len = len + 1;
+len += 1;
 }
 i++;
 }
